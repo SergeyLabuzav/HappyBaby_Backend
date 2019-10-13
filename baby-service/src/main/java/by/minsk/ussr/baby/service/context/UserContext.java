5549197149fba508fc8a8b231bcfbc69e0c33334
Sergey Labuzov <sergeylabuzav@gmail.com>
2019-10-13 21:03:31 +0300

    Updated UserContext Service.

--- baby-service/src/main/java/by/minsk/ussr/baby/service/context/UserContext.java
+++ baby-service/src/main/java/by/minsk/ussr/baby/service/context/UserContext.java
@@ -1,23 +1,18 @@
 package by.minsk.ussr.baby.service.context;
 
-import by.minsk.ussr.auth.api.dto.UserProfileDto;
 import by.minsk.ussr.baby.error.NoUserLoggedInException;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserContext {
 
-    private UserProfileDto userProfile() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    public Integer userProfileId() {
+        OAuth2Authentication authentication = (OAuth2Authentication) SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
             throw new NoUserLoggedInException();
         }
-        return (UserProfileDto) authentication.getDetails();
-    }
-
-    public Integer userProfileId() {
-        return userProfile().getId();
+        return (Integer) authentication.getUserAuthentication().getPrincipal();
     }
 }
